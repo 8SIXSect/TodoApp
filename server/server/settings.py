91b58c738bdab612dabf926868c7d359a5627ac3
Hunter G <hunterg@pop-os.localdomain>
2024-03-07 22:39:16 -0600

    Updated for environment varas

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qe)l((l!ovlld7-3b_3#_ab#ias#r^^%&4w*e(!dqng5*rql8m'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["*"]
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_HEADERS = ['*']
+ALLOWED_HOSTS = ["localhost", "todoapp-2igo.onrender.com"]
+CORS_ALLOWED_ORIGINS = ['http://localhost:8080', "https://todoapp-2igo.onrender.com"]
 
 # Application definition
 
